fa1ac905ace4864b2b7660ee72db0d52ddfdc685
Tomas Suarez <tomasa.suarez81@gmail.com>
2025-06-20 16:46:43 -0300

    Initial commit

---
+++ src/main/java/pasteleria/LePettit/LePettitApplication.java
@@ -0,0 +1,13 @@
+package pasteleria.LePettit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LePettitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LePettitApplication.class, args);
+	}
+
+}
